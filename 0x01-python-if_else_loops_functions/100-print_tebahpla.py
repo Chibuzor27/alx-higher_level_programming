8d50ae2b0f9d9bc0bc54faf6c1420775d9bcade7
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-08-08 19:41:37 -0700

    style

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 flip = False
 for i in range(97+26-1, 97-1, -1):
-    if flip == False:
+    if flip is False:
         print("{0:c}".format(i), end="")
         flip = not flip
     else:
