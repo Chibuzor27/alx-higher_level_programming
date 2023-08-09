4b06859e928e91164e13c66c8c96e2b2823e3802
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-08-08 20:12:29 -0700

    fix zero

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     s = ""
-    if n > 0:
+    if n >= 0:
         for i in range(0, len(str)):
             if i != n:
                 s = s + str[i:i+1]
