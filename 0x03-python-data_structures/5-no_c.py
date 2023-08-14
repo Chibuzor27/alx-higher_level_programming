68fa98537b45c4b4d0524075c3f9ef24488700eb
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-08-14 08:17:57 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     if len(my_string) > 0:
         text = ""
-        for i in range (0, len(my_string)):
+        for i in range(0, len(my_string)):
             if not (my_string[i] == 'c' or my_string[i] == 'C'):
                 text = text + my_string[i]
         return text
