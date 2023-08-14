e7e77bae25a882dd49b078bc86ff68ae6d026bfb
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-08-14 10:05:24 -0700

    update git push
    /

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     dmax = len(tuple_a)
+    new_tuple = ()
     if len(tuple_b) > len(tuple_a):
         dmax = len(tuple_b)
-    new_tuple = tuple([
-        (tuple_a[i] if i < len(tuple_a) else 0) +
-        (tuple_b[i] if i < len(tuple_b) else 0)
-        for i in range(0, dmax)])
+    for i in range(0, dmax):
+        if len(tuple_a) > i and len(tuple_b) > i:
+            new_tuple = new_tuple + ((tuple_a[i] + tuple_b[i]),)
+        elif len(tuple_a) <= i:
+            new_tuple = new_tuple + (tuple_b[i],)
+        else:
+            new_tuple = new_tuple + (tuple_a[i],)
+
     return new_tuple
