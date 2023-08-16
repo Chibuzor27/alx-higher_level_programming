c83469594e542df60c64857ea9a9a32d1dfa76c2
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-08-16 15:42:45 -0700

    0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = [
+            (matrix[i][j])**2
+            for i in range(0, len(matrix)) for j in range(0, len(matrix[i]))
+            ]
+    return new_matrix
