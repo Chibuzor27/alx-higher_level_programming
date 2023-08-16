b6b469b0232863c1268c66ab405bb2e9c6ef92d2
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-08-16 15:48:47 -0700

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [
-            (matrix[i][j])**2
-            for i in range(0, len(matrix)) for j in range(0, len(matrix[i]))
-            ]
+    new_matrix = []
+    for i in range(0, len(matrix)):
+        s = []
+        for j in range(0, len(matrix[i])):
+            s.append(matrix[i][j]**2)
+        new_matrix.append(s)
     return new_matrix
