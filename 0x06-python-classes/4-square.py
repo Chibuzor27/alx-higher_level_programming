f0ba4b94f11d972cadfb6740cbc24d84fdb15323
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-08-29 14:15:36 -0700

    up

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Classes"""
+
+
+class Square:
+    """Square"""
+    __size = 0
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise ValueError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """Area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Get size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size"""
+        if type(value) != int:
+            raise ValueError('size must be an integer')
+
+        if value < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = value
