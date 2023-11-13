e42310531b103dd46b0871c9a53981aa87fffad2
Chibuzor <chibuzornwankwo@ymail.com>
2023-11-13 13:06:34 -0800

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 """Add module"""
 
 
@@ -7,7 +7,7 @@ def add_integer(a, b=None):
     if a is None or (type(a) != int and type(a) != float):
         raise TypeError('a must be an integer')
     if b is None:
-        return a
+        return int(a)
     if type(b) != int and type(b) != float:
         raise TypeError('b must be an integer')
     return int(a) + int(b)
