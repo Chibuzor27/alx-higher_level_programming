051b1196c26f5602e44de301ba0171edd52a98ab
Chibuzor <chibuzornwankwo@ymail.com>
2023-11-14 03:03:39 -0800

    update

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Indent module"""
+
+
+def text_indentation(text):
+    """Indent text"""
+
+    if text is None or type(text) != str:
+        raise TypeError('text must be a string')
+
+    if len(text) == 0:
+        return
+
+    line = 0
+    space = -1
+    for i in range(0, len(text)):
+        if line == 1 and space == 0:
+            print(f'\n\n', end="")
+            line = 0
+            space = 0
+            continue
+
+        if space == -1 and text[i] == ' ':
+            continue
+
+        if space == 0 and text[i] == ' ':
+            space = 1
+            continue
+
+        if space == 1 and text[i] == ' ':
+            continue
+
+        if space == 1 and text[i] != ' ' \
+                and text[i] != '.' and text[i] != '?' and text[i] != ':':
+            print(' ', end="")
+
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print(text[i], end="")
+            line = 1
+            space = 0
+            continue
+
+        print(text[i], end="")
+        space = 0
