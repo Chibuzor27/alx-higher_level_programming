35746d52bae3be8a4e690f9872481fe032dfb485
Chibuzor <chibuzornwankwo@ymail.com>
2023-11-14 04:18:35 -0800

    update

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """Unit test for max integer"""
+    
+    def test_default(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_empty(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_none(self):
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_number(self):
+        with self.assertRaises(TypeError):
+            max_integer([1, 2, 4.2, "m"])
+
+    def test_number(self):
+        with self.assertRaises(TypeError):
+            max_integer(1)
+
+    def test_missing_param(self):
+        self.assertEqual(max_integer(), None)
+
+    def test_invalid_param(self):
+        self.assertEqual(max_integer("az"), "z")
+
+if __name__ == '__main__':
+    unittest.main()
