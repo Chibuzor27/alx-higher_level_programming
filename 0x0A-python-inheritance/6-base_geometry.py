b02574f5cbc9d0ea05bed779cd2e3c6466fc700b
Chibuzor <chibuzornwankwo@ymail.com>
2023-11-19 07:40:32 -0800

    update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class BaseGeometry:
+    """Base Geometry class"""
+
+    def area(self):
+        """Area"""
+        raise Exception('area() is not implemented')
