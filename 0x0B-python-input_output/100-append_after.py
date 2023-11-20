54c4aefe9b026c45f63d37b4d7c6f6d7da0c15e6
Chibuzor <chibuzornwankwo@ymail.com>
2023-11-20 12:38:20 -0800

    update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Append after"""
+    pg = ""
+    if filename != "" or filename != None:
+        with open(filename, encoding="utf-8") as f:
+            line = None
+            while line != "":
+                line = f.readline()
+                pg = pg + line
+                if search_string in line:
+                    pg = pg + new_string
+
+        with open(filename, "w", encoding="utf-8") as n:
+            n.write(pg)
