caaecefe66bd73c3c279b982a1bc0b74e6a4ea10
Chibuzor <chibuzornwankwo@ymail.com>
2023-11-20 13:56:45 -0800

    update

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+import sys
+
+
+count = 0
+size = 0
+errors = {}
+try:
+    for line in sys.stdin:
+        arr = line.replace('\n', '').split(' ')
+        code = int(arr[7])
+        filesize = int(arr[8])
+        count = count + 1
+        size = size + filesize
+
+        if code not in errors.keys():
+            errors[code] = 1
+        else:
+            errors[code] = errors[code] + 1
+
+        if count == 10:
+            print('File size: {}'.format(size))
+            for key, value in sorted(errors.items()):
+                print('{}: {}'.format(key, value))
+            count = 0
+            errors = {}
+            size = 0
+except KeyboardInterrupt:
+    sys.stdout.flush()
+    print('File size: {}'.format(size))
+    for key, value in sorted(errors.items()):
+        print('{}: {}'.format(key, value))
