bb3f977c389ce58e9b9b87ddac9965373811c744
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-11-29 13:58:25 -0800

    update

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Unit test for base class"""
+
+
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """Unit test for base"""
+    
+    def test_constructor(self):
+        base = Base()
+        self.assertEqual(base.id, 1)
+
+        base = Base()
+        self.assertEqual(base.id, 2)
+
+    def test_constructor_withValue(self):
+        base = Base(10)
+        self.assertEqual(base.id, 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
