0e853751edd4c1800f65b553269d7f4ee3c93912
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-11-29 15:51:04 -0800

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,4 +91,4 @@ class Rectangle(Base):
     def __str__(self):
         """String representation"""
         return "[Rectangle] ({}) {}/{} - {}/{}\
-                ".format(self.id, self.x, self.y, self.width, self.height)
+".format(self.id, self.x, self.y, self.width, self.height)
