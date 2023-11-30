6e2e58c8288b54aa0542b372d8e0b724dd89983a
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-11-29 17:54:38 -0800

    update

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set size"""
+        self.validate("width", value)
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """String repre sentation"""
+        return "[Square] ({}) {}/{} - {}\
+".format(self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """Update instance variables"""
+
+        size = len(args)
+        if size > 0:
+            a = 0
+            while a < size and a < 4:
+                if a == 0:
+                    self.id = args[a]
+                elif a == 1:
+                    self.size = args[a]
+                elif a == 2:
+                    self.x = args[a]
+                elif a == 3:
+                    self.y = args[a]
+                a = a + 1
+        else:
+            if kwargs is not None:
+                for (name, value) in kwargs.items():
+                    setattr(self, name, value)
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,198 +1,157 @@
 #!/usr/bin/python3
-"""Rectangle tests"""
+"""Square tests"""
 
 
 import unittest
-from models.rectangle import Rectangle
+from models.square import Square
 
 
-class TestRectangle(unittest.TestCase):
-    """Unit tests for rectangle"""
+class TestSquare(unittest.TestCase):
+    """Unit tests for squaree"""
 
     def test_parameterless_constructor(self):
-        r = Rectangle(1, 1)
-        self.assertEqual(r.id, Rectangle._Base__nb_objects)
+        r = Square(1)
+        self.assertEqual(r.id, Square._Base__nb_objects)
         self.assertEqual(r.height, 1)
         self.assertEqual(r.width, 1)
 
     def test_constructor(self):
-        r = Rectangle(1, 2, 3, 4, 20)
+        r = Square(1, 3, 4, 20)
         self.assertEqual(r.id, 20)
         self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 2)
+        self.assertEqual(r.height, 1)
         self.assertEqual(r.x, 3)
         self.assertEqual(r.y, 4)
 
-    def test_invalid_width(self):
+    def test_invalid_size(self):
         with self.assertRaises(TypeError):
-            r = Rectangle("x", 1)
+            r = Square("x")
 
-    def test_zero_width(self):
+    def test_zero_size(self):
         with self.assertRaises(ValueError):
-            r = Rectangle(0, 1)
+            r = Square(0)
 
-    def test_negative_width(self):
+    def test_negative_size(self):
         with self.assertRaises(ValueError):
-            r = Rectangle(-1, 1)
-
-    def test_invalid_height(self):
-        with self.assertRaises(TypeError):
-            r = Rectangle(1, "x")
-
-    def test_zero_height(self):
-        with self.assertRaises(ValueError):
-            r = Rectangle(1, 0)
-
-    def test_negative_height(self):
-        with self.assertRaises(ValueError):
-            r = Rectangle(1, -1)
+            r = Square(-1)
 
     def test_invalid_x(self):
         with self.assertRaises(TypeError):
-            r = Rectangle(1, 1, "x", 1)
+            r = Square(1, "x", 1)
 
     def test_zero_x(self):
-        r = Rectangle(1, 1, 0, 1)
+        r = Square(1, 0, 1)
         self.assertEqual(r.x, 0)
 
     def test_negative_x(self):
         with self.assertRaises(ValueError):
-            r = Rectangle(1, 1, -1, 1)
+            r = Square(1, -1, 1)
 
     def test_invalid_y(self):
         with self.assertRaises(TypeError):
-            r = Rectangle(1, 1, 1, "x")
+            r = Square(1, 1, "x")
 
     def test_zero_y(self):
-        r = Rectangle(1, 1, 1, 0)
+        r = Square(1, 1, 0)
         self.assertEqual(r.y, 0)
 
     def test_negative_y(self):
         with self.assertRaises(ValueError):
-            r = Rectangle(1, 1, 1, -1)
+            r = Square(1, 1, -1)
 
     def test_area(self):
-        r = Rectangle(2, 5)
-        self.assertEqual(r.area(), 10)
+        r = Square(2)
+        self.assertEqual(r.area(), 4)
 
     def test_string(self):
-        r = Rectangle(1, 2, 3, 4, 5)
-        self.assertEqual(r.__str__(), "[Rectangle] (5) 3/4 - 1/2")
+        r = Square(2, 3, 4, 5)
+        self.assertEqual(r.__str__(), "[Square] (5) 3/4 - 2")
 
     def test_update_nil(self):
-        r = Rectangle(1, 1, 1, 1, 10)
+        r = Square(1, 1, 1, 10)
         r.update()
         self.assertEqual(r.id, 10)
         self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 1)
         self.assertEqual(r.x, 1)
         self.assertEqual(r.y, 1)
 
     def test_update_id(self):
-        r = Rectangle(1, 1, 1, 1, 10)
+        r = Square(1, 1, 1, 10)
         r.update(20)
         self.assertEqual(r.id, 20)
         self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 1)
         self.assertEqual(r.x, 1)
         self.assertEqual(r.y, 1)
 
     def test_update_width(self):
-        r = Rectangle(1, 1, 1, 1, 10)
+        r = Square(1, 1, 1, 10)
         r.update(20, 30)
         self.assertEqual(r.id, 20)
         self.assertEqual(r.width, 30)
-        self.assertEqual(r.height, 1)
-        self.assertEqual(r.x, 1)
-        self.assertEqual(r.y, 1)
-
-    def test_update_height(self):
-        r = Rectangle(1, 1, 1, 1, 10)
-        r.update(20, 30, 40)
-        self.assertEqual(r.id, 20)
-        self.assertEqual(r.width, 30)
-        self.assertEqual(r.height, 40)
         self.assertEqual(r.x, 1)
         self.assertEqual(r.y, 1)
 
     def test_update_x(self):
-        r = Rectangle(1, 1, 1, 1, 10)
-        r.update(20, 30, 40, 50)
+        r = Square(1, 1, 1, 10)
+        r.update(20, 30, 50)
         self.assertEqual(r.id, 20)
         self.assertEqual(r.width, 30)
-        self.assertEqual(r.height, 40)
         self.assertEqual(r.x, 50)
         self.assertEqual(r.y, 1)
 
     def test_update_y(self):
-        r = Rectangle(1, 1, 1, 1, 10)
-        r.update(20, 30, 40, 50, 60)
+        r = Square(1, 1, 1, 10)
+        r.update(20, 30, 50, 60)
         self.assertEqual(r.id, 20)
         self.assertEqual(r.width, 30)
-        self.assertEqual(r.height, 40)
         self.assertEqual(r.x, 50)
         self.assertEqual(r.y, 60)
 
     def test_update_extra(self):
-        r = Rectangle(1, 1, 1, 1, 10)
-        r.update(20, 30, 40, 50, 60, 70)
+        r = Square(1, 1, 1, 10)
+        r.update(20, 30, 50, 60, 70)
         self.assertEqual(r.id, 20)
         self.assertEqual(r.width, 30)
-        self.assertEqual(r.height, 40)
         self.assertEqual(r.x, 50)
         self.assertEqual(r.y, 60)
 
     def test_update_attr_id(self):
-        r = Rectangle(1, 1, 1, 1, 10)
+        r = Square(1, 1, 1, 10)
         r.update(id=20)
         self.assertEqual(r.id, 20)
         self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 1)
         self.assertEqual(r.x, 1)
         self.assertEqual(r.y, 1)
 
     def test_update_attr_width(self):
-        r = Rectangle(1, 1, 1, 1, 10)
+        r = Square(1, 1, 1, 10)
         r.update(width=20)
         self.assertEqual(r.id, 10)
         self.assertEqual(r.width, 20)
-        self.assertEqual(r.height, 1)
-        self.assertEqual(r.x, 1)
-        self.assertEqual(r.y, 1)
-
-    def test_update_attr_height(self):
-        r = Rectangle(1, 1, 1, 1, 10)
-        r.update(height=20)
-        self.assertEqual(r.id, 10)
-        self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 20)
         self.assertEqual(r.x, 1)
         self.assertEqual(r.y, 1)
 
     def test_update_attr_x(self):
-        r = Rectangle(1, 1, 1, 1, 10)
+        r = Square(1, 1, 1, 10)
         r.update(x=20)
         self.assertEqual(r.id, 10)
         self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 1)
         self.assertEqual(r.x, 20)
         self.assertEqual(r.y, 1)
 
     def test_update_attr_y(self):
-        r = Rectangle(1, 1, 1, 1, 10)
+        r = Square(1, 1, 1, 10)
         r.update(y=20)
         self.assertEqual(r.id, 10)
         self.assertEqual(r.width, 1)
-        self.assertEqual(r.height, 1)
         self.assertEqual(r.x, 1)
         self.assertEqual(r.y, 20)
 
     def test_update_attr_all(self):
-        r = Rectangle(1, 1, 1, 1, 10)
-        r.update(id=20, height=20, width=20, x=20, y=20, unknown=20)
+        r = Square(1, 1, 1, 10)
+        r.update(id=20, width=20, x=20, y=20, unknown=20)
         self.assertEqual(r.id, 20)
         self.assertEqual(r.width, 20)
-        self.assertEqual(r.height, 20)
         self.assertEqual(r.x, 20)
         self.assertEqual(r.y, 20)
 
