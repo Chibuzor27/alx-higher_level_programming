a2a86d18bd90f7969fede73445ce235299dcffd4
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2023-11-29 18:47:10 -0800

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """Base test"""
 
 
+import json
+
+
 class Base():
     """ The base class """
 
@@ -14,3 +17,10 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Convert to json string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,7 +8,7 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     """Unit test for base"""
-    
+
     def test_constructor(self):
         base = Base()
         self.assertEqual(base.id, 1)
@@ -19,6 +19,11 @@ class TestBase(unittest.TestCase):
     def test_constructor_withValue(self):
         base = Base(10)
         self.assertEqual(base.id, 10)
+
+    def test_to_dictionaries_json(self):
+        base = Base()
+        dic = {'x': 2, 'width': 10}
+        self.assertEqual(Base.to_json_string(dic), '{"x": 2, "width": 10}')
 
 
 if __name__ == '__main__':
