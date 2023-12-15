db236b8bc57f10b7f3ffb3b250fa96eabfe0ba8f
Chibuzor <chibuzornwankwo@ymail.com>
2023-12-15 11:45:49 -0800

    update

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- List genres (Dexter)
-SELECT c.name AS genre FROM tv_shows a
+SELECT c.name FROM tv_shows a
 JOIN tv_show_genres b ON a.id = b.show_id
 JOIN tv_genres c ON b.genre_id = c.id
 WHERE title = 'Dexter'
