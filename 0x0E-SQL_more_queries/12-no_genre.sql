0d03c11c8ae10a7b94a6604d5fe56f477e5414b4
Chibuzor <chibuzornwankwo@ymail.com>
2023-12-15 11:13:46 -0800

    up

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,5 @@
 -- List shows without a genre
 SELECT title, genre_id FROM tv_shows a
-LEFT OUTER JOIN tv_show_genres b ON a.id = b.show_id AND genre_id = NULL
+LEFT OUTER JOIN tv_show_genres b ON a.id = b.show_id
+WHERE genre_id is NULL
 ORDER BY title, genre_id;
