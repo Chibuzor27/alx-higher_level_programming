d998e98fbce056144126d346fddaf193bdf75f96
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2024-01-13 07:28:28 -0800

    update

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+if (process.argv[2] === undefined || process.argv[3] === undefined) {
+  console.log(0);
+} else {
+  console.log(process.argv.slice(2).sort((a, b) => b - a)[1]);
+}
