d4fe083c5d457aa3f9d54822335eb7eebfbf8dd7
Chibuzor Uzondu <chibuzornwankwo@ymail.com>
2024-01-13 09:16:10 -0800

    update

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+let ret = {};
+for (const key in dict) {
+  if (!(dict[key] in ret)) {
+    ret[dict[key]] = [];
+  }
+  ret[dict[key]].push(key);
+}
+console.log(ret);
